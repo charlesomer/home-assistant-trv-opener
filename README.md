ad55b57de3c053b5222a7f55f0141fb936464e49
Charles Omer <charles@zyndex.co.uk>
2021-02-08 18:04:10 +0000

    First commit.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Home Assistant TRV Opener
+_Tested with the Drayton Wiser Heating System_
+
+## Requirements
+ - Pyscript for Home Assistant
+ - Heating system needs to be integrated with Home Assistant.
+
+Once Pyscript is installed, add the script from this repo to the Pyscript folder (e.g. `volumes/home-assistant/pyscript/trv-script.py`). If you are not using the Wiser heat system, you may need to change the variables set at the beginning to work with your system.
+
+By default, this script will increase the temperature of TRVs which are above temperature to 25 degrees to force them open and will then reset them back to the schedule once all minimum temperatures have been reached. This value can be changed in the script.
+
+Then, Home Assistant needs to know when to run this script. For now, I am running it every minute but it would be nicer if it could be run only when the TRVs detect a change in temperature for example.
+
+To run every minute:
+ 1. Navigate to the Home Assistant GUI
+ 2. Configuration
+ 3. Automations
+ 4. Add Automation
+ 5. Start with an empty automation
+ 6. Edit via the YAML and use something like this:
+ ```
+alias: TRV Monitor
+description: ''
+trigger:
+- platform: time_pattern
+    minutes: /1
+condition: []
+action:
+- service: pyscript.trv_monitor
+    data: {}
+mode: single
+ ```
+You may need to go to Developer Tools > Services > Look for `Pyscript.reload` and call this service before the `trv_monitor` function appears.
---
+++ trv-script.py
@@ -0,0 +1,74 @@
+# Identifiers for TRV temperatures.
+current_temperature_identifier = "current_temperature"
+target_temperature_identifier  = "temperature"
+
+service_name_to_set_temperatures = "set_temperature"
+
+# The temperature to set TRVs to to force them open.
+temperature_used_to_open_trv = 25
+
+attribute_to_check_trv       = "preset_mode"
+attribute_to_check_trv_value = "boost"
+
+service_to_use_to_reset_trvs       = "set_preset_mode"
+service_to_use_to_reset_trvs_value = "Cancel Boost"
+
+@service
+def trv_monitor():
+  # Get array of rooms with trvs.
+  trv_rooms = state.names("climate")
+
+  # We need to find out if any TRV is supposed to be on, i.e. if the current
+  # temperature is below the target temperature.
+
+  isHeatingRequired = False
+
+  for room in trv_rooms:
+    # Get attributes for the room.
+    attributes = state.getattr(room)
+
+    current_temperature = attributes[current_temperature_identifier]
+    target_temperature  = attributes[target_temperature_identifier]
+
+    if (
+      current_temperature < target_temperature and
+      attributes[attribute_to_check_trv] == attribute_to_check_trv_value
+    ):
+      isHeatingRequired = True
+      # There is a TRV which should be heating, we can now exit this loop
+      # and loop again and this time set TRVs which aren't supposed to be
+      # heating to a higher temperature to force them open.
+      break
+
+  if isHeatingRequired == True:
+    log.info("Heating is required.")
+    for room in trv_rooms:
+      attributes = state.getattr(room)
+
+      current_temperature = attributes[current_temperature_identifier]
+      target_temperature  = attributes[target_temperature_identifier]
+
+      # This time, if the current temperature is higher than the target
+      # temperature (when the TRV will be off), we can increase the target
+      # temperature to force the TRV open.
+      if (
+        current_temperature > target_temperature and
+        attributes[attribute_to_check_trv] != attribute_to_check_trv_value
+      ):
+        service.call("climate", service_name_to_set_temperatures,
+          entity_id=room,
+          temperature=temperature_used_to_open_trv
+        )
+        log.info("Setting TRV for: " + room)
+  else:
+    log.info("Heating is not required.")
+    # Make sure to reset any TRVs that were previously set to a high
+    # back to their defaults.
+    for room in trv_rooms:
+      attributes = state.getattr(room)
+      if attributes[attribute_to_check_trv] != attribute_to_check_trv_value:
+        service.call("climate", service_to_use_to_reset_trvs,
+          entity_id=room,
+          preset_mode=service_to_use_to_reset_trvs_value
+        )
+        log.info("Resetting TRV for: " + room)
