a78f42d573d4b0e6188aea0e7a62eb241c687dd8
Charles <32498151+charlesomer@users.noreply.github.com>
2021-11-18 20:47:27 +0000

    New version using vars.

--- trv-script.py
+++ trv-script.py
@@ -1,4 +1,8 @@
+from ast import literal_eval
+
 # Identifiers for TRV temperatures.
+state.persist('pyscript.script_boosted_entities')
+
 current_temperature_identifier = "current_temperature"
 target_temperature_identifier  = "temperature"
 
@@ -8,26 +12,26 @@ service_name_to_set_temperatures = "set_temperature"
 # I've found that 21 degrees is a good balance between forcing them open
 # and rooms getting too hot but this may need adjusting based on your
 # preference.
-temperature_used_to_open_trv = 21
+temperature_used_to_open_trv = 20
 
-attribute_trv_when_manually_opened = "preset_mode"
-attribute_trv_when_manually_opened_value = "Override"
+attribute_trv_preset_mode = "preset_mode"
+attribute_trv_when_manually_opened_value = "boost"
 
 attribute_demand_check = "percentage_demand"
 
 service_to_use_to_reset_trvs       = "set_preset_mode"
 service_to_use_to_reset_trvs_value = "Cancel Boost"
 
+# pyscript.script_boosted_entities = []
+
 @service
 def trv_monitor():
   # Get array of rooms with trvs.
   trv_rooms = state.names("climate")
 
-  # We need to find out if any TRV is supposed to be on, i.e. if the current
-  # temperature is below the target temperature.
+  # We need to find out if a user (not this script) has manually increased the temperature.
 
   isHeatingRequired = False
-
   for room in trv_rooms:
     # Get attributes for the room.
     attributes = state.getattr(room)
@@ -36,10 +40,11 @@ def trv_monitor():
     target_temperature  = attributes[target_temperature_identifier]
 
     if (
-      current_temperature < target_temperature and
-      attributes[attribute_trv_when_manually_opened] != attribute_trv_when_manually_opened_value and
-      attributes[attribute_demand_check] > 0
+      attributes[attribute_demand_check] > 0 and
+      (room not in literal_eval(pyscript.script_boosted_entities)) and
+      attributes['hvac_action'] != 'idle'
     ):
+      # Check if this has been set by the script or not.
       isHeatingRequired = True
       # There is a TRV which should be heating, we can now exit this loop
       # and loop again and this time set TRVs which aren't supposed to be
@@ -48,6 +53,15 @@ def trv_monitor():
 
   if isHeatingRequired == True:
     log.info("Heating is required.")
+
+    tmp_script_boosted_entities = literal_eval(pyscript.script_boosted_entities)
+    for room in literal_eval(pyscript.script_boosted_entities):
+      attributes = state.getattr(room)
+      if attributes['preset_mode'] != 'boost':
+         tmp_script_boosted_entities.remove(room)
+    pyscript.script_boosted_entities = tmp_script_boosted_entities
+
+    script_boosted_entities = []
     for room in trv_rooms:
       attributes = state.getattr(room)
 
@@ -58,26 +72,25 @@ def trv_monitor():
       # temperature (when the TRV will be off), we can increase the target
       # temperature to force the TRV open.
       if (
-        current_temperature > target_temperature and
-        attributes[attribute_trv_when_manually_opened] != attribute_trv_when_manually_opened_value and
-        attributes[attribute_demand_check] == 0
+        attributes[attribute_demand_check] == 0 and
+        (room not in literal_eval(pyscript.script_boosted_entities))
       ):
         service.call("climate", service_name_to_set_temperatures,
-          entity_id=room,
-          temperature=temperature_used_to_open_trv
+         entity_id=room,
+         temperature=temperature_used_to_open_trv
         )
+        script_boosted_entities.append(room)
         log.info("Setting TRV for: " + room)
+    pyscript.script_boosted_entities = literal_eval(pyscript.script_boosted_entities) + script_boosted_entities
   else:
     log.info("Heating is not required.")
     # Make sure to reset any TRVs that were previously set to a high
     # back to their defaults.
     for room in trv_rooms:
       attributes = state.getattr(room)
-      if (
-        attributes[attribute_trv_when_manually_opened] == attribute_trv_when_manually_opened_value
-      ):
-        service.call("climate", service_to_use_to_reset_trvs,
-          entity_id=room,
-          preset_mode=service_to_use_to_reset_trvs_value
-        )
-        log.info("Resetting TRV for: " + room)
+      service.call("climate", service_to_use_to_reset_trvs,
+        entity_id=room,
+        preset_mode=service_to_use_to_reset_trvs_value
+      )
+      log.info("Resetting TRV for: " + room)
+    pyscript.script_boosted_entities = []
